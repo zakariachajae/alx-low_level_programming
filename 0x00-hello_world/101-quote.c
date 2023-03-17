1e94a688283c41635f5104e1892c2660f1ea0e49
zakariachajae <zakariachajae@gmail.com>
2023-03-17 09:09:59 -0700

    dsdsd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, 46);
 return (1);
