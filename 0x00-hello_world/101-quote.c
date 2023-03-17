da8983b17d96cdec85ec1f3cf3c24b0a1cb7c184
zakariachajae <zakariachajae@gmail.com>
2023-03-17 09:08:41 -0700

    dsdsd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 46);
+return (1);
+}
