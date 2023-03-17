5ff52fdbbb47dd30e924d955237076c3d8dac4ad
zakariachajae <zakariachajae@gmail.com>
2023-03-17 09:46:07 -0700

    dsdsd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  */
 int main(void)
 {
-char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char *message = "and that piece of art is useful\""
+	" - Dora Korpar, 2015-10-19\n";
 write(2, message, 59);
 
-return (0);
+return (1);
 }
