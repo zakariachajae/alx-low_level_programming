e693ebcee5f00076f4fb196dc932804e7d872b63
zakariachajae <zakariachajae@gmail.com>
2023-03-20 06:11:43 -0700

    sqsqs

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0; digit < 10; digit++)
+{
+putchar(digit + '0');
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
