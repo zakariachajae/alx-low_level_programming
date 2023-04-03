cbdc5ad148581b8cc0d54611a3010f553da7e85f
zakariachajae <zakariachajae@gmail.com>
2023-04-03 15:02:28 -0700

    israe

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
  * Return: pointer to first occurrence of character c in string s, or NULL
  *         if character is not found
  */
-char *_strchr(char *s, char c)
+char *strchr(char *s, char c)
 {
 while (*s != '\0')
 {
