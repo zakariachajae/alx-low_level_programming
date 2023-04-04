697df5b2b99be95ed78a4c71ed6506b5bc3e383d
zakariachajae <zakariachajae@gmail.com>
2023-04-04 04:02:42 -0700

    israe

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,12 +9,13 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+char a;
 unsigned int count = 0;
 int found;
 while (*s)
 {
 found = 0;
-for (char *a = accept; *a; a++)
+for (*a = accept; *a; a++)
 {
 if (*s == *a)
 {
